aa66a1dadf920d93347817c200ab159a0afd9835
vaibhav shah <vaibhavshah96@gmail.com>
2021-02-11 23:23:49 -0500

    Changes to average case

--- Java-Assignments/src/main/java/exercise2/OperatorBasedTask.java
+++ Java-Assignments/src/main/java/exercise2/OperatorBasedTask.java
@@ -19,26 +19,42 @@ public class OperatorBasedTask {
 		
 		Scanner scanner = new Scanner(System.in);
 		int option = scanner.nextInt();
-		System.out.println("Enter numbers: ");
-		int i1 = scanner.nextInt();
-		int i2 = scanner.nextInt();
+		int i1;
+		int i2;
 		
 		scanner.close();
 		switch (option){
 		case 1:
+			System.out.println("Enter numbers: ");
+			i1 = scanner.nextInt();
+			i2 = scanner.nextInt();
 			System.out.print("Add: " + (i1+i2));
 			break;
 		case 2:
+			System.out.println("Enter numbers: ");
+			i1 = scanner.nextInt();
+			i2 = scanner.nextInt();
 			System.out.print("Sub: " + (i1 - i2));
 			break;
 		case 3:
+			System.out.println("Enter numbers: ");
+			i1 = scanner.nextInt();
+			i2 = scanner.nextInt();
 			System.out.print("Div: " + (i1/i2));
 			break;
 		case 4:
+			System.out.println("Enter numbers: ");
+			i1 = scanner.nextInt();
+			i2 = scanner.nextInt();
 			System.out.print("Mul: " + (i1*i2));
 			break;
 		case 5:
-			System.out.print("Avg: " + (float)(i1+i2)/2);
+			System.out.println("Enter 4 numbers: ");
+			i1 = scanner.nextInt();
+			i2 = scanner.nextInt();
+			int first1 = scanner.nextInt();
+			int first2 = scanner.nextInt();
+			System.out.print("Avg: " + (float)(i1+i2+first1+first2)/4);
 			break;
 		default:
 			System.out.println("Enter valid operation.");
