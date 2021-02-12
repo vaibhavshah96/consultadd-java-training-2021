4b494c11d4d6e784a1e139b4c569a6094a3a624e
vaibhav shah <vaibhavshah96@gmail.com>
2021-02-11 20:12:54 -0500

    Removed comments

--- Java-Assignments/src/main/java/exercise1/SwapNumbers.java
+++ Java-Assignments/src/main/java/exercise1/SwapNumbers.java
@@ -2,17 +2,6 @@ package exercise1;
 
 import java.util.Scanner;
 
-//Swap two numbers using third variable as result name and do the same task without using any third variable. 
-//
-//2. 	Write a program to print the value given by the user. 
-//
-//3. 	Write a program to complete the task given below: 
-//
-//Ask the user to enter any 2 numbers in between 1-10 and add both of them to another variable call z. 
-//
-//Use z for adding 30 into it and print the final result by using variable results. 
-
-
 public class SwapNumbers {
 	
 	public static void main(String[] args) {
