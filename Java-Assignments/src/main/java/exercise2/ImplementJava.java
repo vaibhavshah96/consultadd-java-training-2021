2fa36c970dfd29cbde52e7ce8e2afb810fae3414
vaibhav shah <vaibhavshah96@gmail.com>
2021-02-12 12:38:35 -0500

    Added flow chart implementation with correct indent

--- Java-Assignments/src/main/java/exercise2/ImplementJava.java
+++ Java-Assignments/src/main/java/exercise2/ImplementJava.java
@@ -3,54 +3,51 @@ package exercise2;
 public class ImplementJava {
 
 	public static void main(String[] args) {
-		
+
 		int a = 10;
 		int b = 20;
 		int c = 30;
-		
-		int avg = (a+b+c)/3;
-		
-		boolean justOne = true;
-		
+
+		int avg = (a + b + c) / 3;
+
 		System.out.println("Avg = " + avg);
-		
+
 		boolean greaterThanA = (avg > a);
 		boolean greaterThanB = (avg > b);
 		boolean greaterThanC = (avg > c);
-		
+		boolean justOne = true;
+
 		String result = "";
-		
+
 		if (greaterThanA) {
-				result = "Avg higher than " + "a";
-				if(greaterThanB) {
-					justOne = false;
-					result = result + ",b";
-				}
-				if(greaterThanC) {
-					justOne = false;
-					result = result + ",c";
-				}
-				
-				result = justOne? "Avg just higher than a" : result;
-				
-				
+			result = "Avg higher than " + "a";
+			if (greaterThanB) {
+				justOne = false;
+				result = result + ",b";
 			}
-		
-		else if (greaterThanB) {
-			result = "Avg higher than " + "b";
-			if(greaterThanC) {
+			if (greaterThanC) {
 				justOne = false;
 				result = result + ",c";
-			}	
-			result = justOne? "Avg just higher than b" : result;
+			}
+
+			result = justOne ? "Avg just higher than a" : result;
+
 		}
-		
+
+		else if (greaterThanB) {
+			result = "Avg higher than " + "b";
+			if (greaterThanC) {
+				justOne = false;
+				result = result + ",c";
+			}
+			result = justOne ? "Avg just higher than b" : result;
+		}
+
 		else if (greaterThanC) {
 			result = "Avg just higher than c";
 		}
-		
-		
+
 		System.out.println(result);
-		}
+	}
 
 }
