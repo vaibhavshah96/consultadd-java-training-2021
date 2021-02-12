1a07bbaf6cec75e9e0a702989518bcf918a1391e
vaibhav shah <vaibhavshah96@gmail.com>
2021-02-12 12:58:45 -0500

    Added print 1 to 6 without 3 and 6

---
+++ Java-Assignments/src/main/java/exercise2/PrintRequiredNumbers.java
@@ -0,0 +1,15 @@
+package exercise2;
+
+public class PrintRequiredNumbers {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i<=6; i++) {
+			if (i != 3 && i != 6) {
+				System.out.print(i + " ");
+			}
+		}
+
+	}
+
+}
