f8d933d62199180e3ff9e4e0d7a518d9da2b2e68
vaibhav shah <vaibhavshah96@gmail.com>
2021-02-17 18:48:44 -0500

    Added sum and average of 10 numbers.

--- Java-Assignments/src/main/java/exercise3/AcceptAndSum10Numbers.java
+++ Java-Assignments/src/main/java/exercise3/AcceptAndSum10Numbers.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class AcceptAndSum10Numbers {
 
 	public static void main(String[] args) {
-		
-		int sum = 0; 
+
+		int sum = 0;
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter 10 numbers:");
 		for (int i = 0; i < 10; i++) {
 			sum = sum + scanner.nextInt();
 		}
-		System.out.println("Sum is " + sum + " and average is " + sum/10);
+		System.out.println("Sum is " + sum + " and average is " + sum / 10);
 		scanner.close();
-		
+
 	}
 
 }
