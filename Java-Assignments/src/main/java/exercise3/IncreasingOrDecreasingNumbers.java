26a65396b75b283ffd21d150d07ca215fa6030e9
vaibhav shah <vaibhavshah96@gmail.com>
2021-02-18 22:45:18 -0500

    Added increasing or decreasing numbers.

--- Java-Assignments/src/main/java/exercise3/IncreasingOrDecreasingNumbers.java
+++ Java-Assignments/src/main/java/exercise3/IncreasingOrDecreasingNumbers.java
@@ -5,36 +5,34 @@ import java.util.Scanner;
 public class IncreasingOrDecreasingNumbers {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("Enter number one: ");
 		int num1 = scanner.nextInt();
 		System.out.print("\nEnter number two: ");
 		int num2 = scanner.nextInt();
 		System.out.print("\nEnter number three: ");
 		int num3 = scanner.nextInt();
-		
+
 		System.out.println(compareNumbers(num1, num2, num3));
-		
+
 		scanner.close();
-		
-		
+
 	}
 
-	private static String compareNumbers (int num1, int num2, int num3) {
+	private static String compareNumbers(int num1, int num2, int num3) {
 		String result = "Neither increasing or decreasing";
-		
+
 		if (num1 > num2) {
-			if (num2>num3) {
+			if (num2 > num3) {
 				result = "Decreasing";
 			}
-		}
-		else if(num1 < num2) {
-			if(num2 < num3) {
+		} else if (num1 < num2) {
+			if (num2 < num3) {
 				result = "Increasing";
 			}
 		}
 		return result;
-	} 
+	}
 }
