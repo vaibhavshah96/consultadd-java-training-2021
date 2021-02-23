3efa775d109298df712f03a9f674374b70d1c5ab
vaibhav shah <vaibhavshah96@gmail.com>
2021-02-23 14:36:48 -0500

    Added remove white spaces.

--- Java-Assignments/src/main/java/exercise4/RemoveWhiteSpacesFromString.java
+++ Java-Assignments/src/main/java/exercise4/RemoveWhiteSpacesFromString.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class RemoveWhiteSpacesFromString {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Enter string: ");
 		String str = scanner.nextLine();
-		
+
 		str = str.replaceAll("\\s", "");
-		
+
 		System.out.println(str);
 		scanner.close();
 	}
