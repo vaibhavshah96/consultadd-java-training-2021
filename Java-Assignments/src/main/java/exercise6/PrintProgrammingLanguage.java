120d30c0502c90b19f6b6bf80602c0abec66c715
vaibhav shah <vaibhavshah96@gmail.com>
2021-02-26 17:00:14 -0500

    Added programming language print.

--- Java-Assignments/src/main/java/exercise6/PrintProgrammingLanguage.java
+++ Java-Assignments/src/main/java/exercise6/PrintProgrammingLanguage.java
@@ -1,25 +1,25 @@
 package exercise6;
 
 class Programming {
-	
+
 	String toPrint = "I love ";
-	
-	Programming(){
+
+	Programming() {
 		toPrint = toPrint + "programming languages";
 		System.out.println(toPrint);
 	}
-	
-	Programming(String addToPrint){
+
+	Programming(String addToPrint) {
 		toPrint = toPrint + addToPrint;
 		System.out.println(toPrint);
 	}
-	
+
 }
 
 public class PrintProgrammingLanguage {
 
 	public static void main(String[] args) {
-		
+
 		Programming p1 = new Programming();
 		Programming p2 = new Programming("Java");
 
