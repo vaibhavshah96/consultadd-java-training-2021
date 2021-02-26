0e810a9176fcdab8b84833dd924c7c79b55815c2
vaibhav shah <vaibhavshah96@gmail.com>
2021-02-26 11:11:47 -0500

    Fixed remove dupes and return length and added remove dupes and add zeros to end.

--- Java-Assignments/src/main/java/exercise5/RemoveDupesAddZero.java
+++ Java-Assignments/src/main/java/exercise5/RemoveDupesAddZero.java
@@ -2,25 +2,30 @@ package exercise5;
 
 import java.util.Arrays;
 
-public class RemoveDupesAndLength {
+public class RemoveDupesAddZero {
 
 	public static void main(String[] args) {
-
+		
 		int[] numbers = { 2, 4, 2, 1, 1, 1, 12, 10, 11, 5, 7 };
 		int j = 0;
 		Arrays.sort(numbers);
-
+		
+		
 		for (int i = 0; i < numbers.length - 1; i++) {
+			
 			if (numbers[i] != numbers[i + 1]) {
 				numbers[j] = numbers[i];
 				j++;
 			}
 		}
+		numbers[j++] = numbers[numbers.length -1];
 		
-		numbers[j++] = numbers[numbers.length - 1];
+		for (int i = j; i < numbers.length; i++) {
+			numbers[i] = 0;
+		}
 
-		System.out.println("Length of non duplicate array = " + (j));
-		for (int i = 0; i < j; i++) {
+		
+		for (int i = 0; i < numbers.length; i++) {
 			System.out.print(numbers[i] + " ");
 		}
 
