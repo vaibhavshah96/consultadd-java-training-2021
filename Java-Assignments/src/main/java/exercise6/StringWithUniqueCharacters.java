b9fa0ce85fec924cfb034269d1b53ff7ecc75efd
vaibhav shah <vaibhavshah96@gmail.com>
2021-03-02 18:54:17 -0500

    Added program to find if all letters in string are unique.

---
+++ Java-Assignments/src/main/java/exercise6/StringWithUniqueCharacters.java
@@ -0,0 +1,29 @@
+package exercise6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringWithUniqueCharacters {
+
+	public static void main(String[] args) {
+
+		String str = "algorithm";
+
+		List<Character> unique = new ArrayList<Character>();
+		boolean flag = true;
+
+		for (char c : str.toCharArray()) {
+			if (unique.contains(c)) {
+				System.out.println("Not all unique.");
+				flag = false;
+				break;
+			} else {
+				unique.add(c);
+			}
+		}
+		if (flag)
+			System.out.println("All unique.");
+
+	}
+
+}
