66f1f2acdc81a6e90fb9846593d4fb220b134141
vaibhav shah <vaibhavshah96@gmail.com>
2021-03-02 19:07:51 -0500

    Added word count.

---
+++ Java-Assignments/src/main/java/exercise6/NumberOfWordsInString.java
@@ -0,0 +1,20 @@
+package exercise6;
+
+public class NumberOfWordsInString {
+
+	public static void main(String[] args) {
+
+		String str = "The quick brown fox jumped over the lazy dog.";
+		int count = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == ' ') {
+				count++;
+			}
+		}
+
+		System.out.println("Number of words is " + (count + 1));
+
+	}
+
+}
